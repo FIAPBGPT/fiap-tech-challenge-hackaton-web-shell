5aa5bc4a3bc9b08737af1b219debea556fc7ddaf
cibelesantos <cibele.santos@oitocreditoimobiliario.com.br>
2025-06-28 19:52:25 -0300

    feat: redirecionando pro login

--- pages/home/index.tsx
+++ pages/home/index.tsx
@@ -1,14 +1,15 @@
 'use client';
 
+import ButtonComponent from "@/@core/components/ui/Button";
 import { MainContainer } from "@/@theme/custom/DashboradPage-styles";
+import Login from "../temporario";
+
 
 
 export default function DashboardPage() {
   return (
     <MainContainer>
-        <div>
-            
-        </div>
+      <Login/>
     </MainContainer>
   );
 }
