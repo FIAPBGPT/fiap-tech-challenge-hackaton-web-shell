1b60621d042da345d0613276ec130f1ee05616b7
Flavia Jaconis <fla_njaconis@yahoo.com.br>
2025-06-29 12:05:27 -0300

    correção estrutura HeaderMenu

--- @core/components/baseHeaderMenu/HeaderMenu.component.tsx
+++ @core/components/baseHeaderMenu/HeaderMenu.component.tsx
@@ -1,18 +1,35 @@
 import HeaderComponent from "@/@core/components/header/Header.component";
 import MenuComponent from "@/@core/menu/Menu.component";
-import { MainContainer } from "@/@theme/custom/DashboradPage-naoSubir-styles";
+import { Container } from "@/@theme/custom/DashboradPage-naoSubir-styles";
 import { useState } from "react";
 
-export default function HeaderMenuComponent({ children }: { children: React.ReactNode }) {
+export default function HeaderMenuComponent({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   return (
-    <MainContainer>
-      <HeaderComponent toggleMenu={() => setIsMenuOpen((visible) => !visible)}/>
-      <MenuComponent isMenuOpen={isMenuOpen} />
-      <main style={{ flex: 1, padding: "20px", width: "100%" }}>
-        {children}
-      </main>
-    </MainContainer>
+    <Container>
+      <HeaderComponent
+        toggleMenu={() => setIsMenuOpen((visible) => !visible)}
+      />
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          width: "100%",
+          height: "100vh",
+        }}
+      >
+        <MenuComponent isMenuOpen={isMenuOpen} />
+        <main
+          style={{ flex: 1, padding: "20px", width: "100%", height: "100vh" }}
+        >
+          {children}
+        </main>
+      </div>
+    </Container>
   );
 }
