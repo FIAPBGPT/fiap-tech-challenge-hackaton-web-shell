cb64213cdfd1d0b60fc8ac510f8ec7e72829a9ef
Flavia Jaconis <fla_njaconis@yahoo.com.br>
2025-07-20 18:44:05 -0300

    Correção Titulo Convidar usuario

--- @core/hooks/useSection.ts
+++ @core/hooks/useSection.ts
@@ -1,71 +1,71 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState } from 'react'
 
 interface Item {
-  item: ItemProps;
+  item: ItemProps
 }
 
 export enum ItemProps {
-  HOME = "home",
-  USUARIO = "usuario",
-  PRODUTO = "produto",
-  ESTOQUE = "estoque",
-  PRODUCAO = "producao",
-  VENDA = "venda",
-  FAZENDA = "fazenda",
-  SAFRA = "safra",
-  METAS = "metas",
+  HOME = 'home',
+  USUARIO = 'usuario',
+  PRODUTO = 'produto',
+  ESTOQUE = 'estoque',
+  PRODUCAO = 'producao',
+  VENDA = 'venda',
+  FAZENDA = 'fazenda',
+  SAFRA = 'safra',
+  METAS = 'metas',
 }
 
 export enum CurrentSection {
-  DASHBORAD = "Sua Dashbord",
-  CAD_PRODUTOS = "Gestão de Produtos",
-  CAD_FAZENDAS = "Gestão de Fazendas",
-  CAD_SAFRAS = "Gestão de Safras",
-  COMPLETE_CADASTRO = "Complete seu cadastro",
-  CAD_ESTOQUE = "Gestão de Estoque",
-  CAD_PRODUCAO = "Gestão de Produção",
-  CAD_VENDAS = "Gestão de Vendas",
-  CAD_METAS = "Gestão de Metas",
+  DASHBORAD = 'Sua Dashbord',
+  CAD_PRODUTOS = 'Gestão de Produtos',
+  CAD_FAZENDAS = 'Gestão de Fazendas',
+  CAD_SAFRAS = 'Gestão de Safras',
+  COMPLETE_CADASTRO = 'Convidar usuário',
+  CAD_ESTOQUE = 'Gestão de Estoque',
+  CAD_PRODUCAO = 'Gestão de Produção',
+  CAD_VENDAS = 'Gestão de Vendas',
+  CAD_METAS = 'Gestão de Metas',
 }
 
 export function useSection(props: Item): CurrentSection {
   const [section, setSection] = useState<CurrentSection>(
     CurrentSection.DASHBORAD
-  );
+  )
 
   useEffect(() => {
     switch (props.item) {
       case ItemProps.HOME:
-        setSection(CurrentSection.DASHBORAD);
-        break;
+        setSection(CurrentSection.DASHBORAD)
+        break
       case ItemProps.PRODUTO:
-        setSection(CurrentSection.CAD_PRODUTOS);
-        break;
+        setSection(CurrentSection.CAD_PRODUTOS)
+        break
       case ItemProps.FAZENDA:
-        setSection(CurrentSection.CAD_FAZENDAS);
-        break;
+        setSection(CurrentSection.CAD_FAZENDAS)
+        break
       case ItemProps.SAFRA:
-        setSection(CurrentSection.CAD_SAFRAS);
-        break;
+        setSection(CurrentSection.CAD_SAFRAS)
+        break
       case ItemProps.USUARIO:
-        setSection(CurrentSection.COMPLETE_CADASTRO);
-        break;
+        setSection(CurrentSection.COMPLETE_CADASTRO)
+        break
       case ItemProps.ESTOQUE:
-        setSection(CurrentSection.CAD_ESTOQUE);
-        break;
+        setSection(CurrentSection.CAD_ESTOQUE)
+        break
       case ItemProps.PRODUCAO:
-        setSection(CurrentSection.CAD_PRODUCAO);
-        break;
+        setSection(CurrentSection.CAD_PRODUCAO)
+        break
       case ItemProps.VENDA:
-        setSection(CurrentSection.CAD_VENDAS);
-        break;
+        setSection(CurrentSection.CAD_VENDAS)
+        break
       case ItemProps.METAS:
-        setSection(CurrentSection.CAD_METAS);
-        break;
+        setSection(CurrentSection.CAD_METAS)
+        break
       default:
-        setSection(CurrentSection.DASHBORAD);
+        setSection(CurrentSection.DASHBORAD)
     }
-  }, [props.item]);
+  }, [props.item])
 
-  return section;
+  return section
 }
